70ee728bb3a6e40b930ab5378873ee607886414b
Ashley Ansell <ansela@users.noreply.github.com>
2019-01-30 21:12:57 -0500

    Revert "WIP: Initial set up (#1)"
    This reverts commit 891732710f588196eaf30a9772d1700f6736ec52.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import React, { Component } from "react";
-
-import NavBar from "./components/NavBar";
-import Content from "./components/Content";
-import Footer from "./components/Footer";
-
-import "./styles/main.scss";
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="app">
-        <NavBar />
-        <Content />
-        <Footer />
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
     );
   }
