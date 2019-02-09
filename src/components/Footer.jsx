12fbe36a2793087db4227900925b6cafc95c50cd
Ashley Ansell <ansela@users.noreply.github.com>
2019-02-08 22:49:14 -0500

    Fix footer on mobile (#16)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,55 +1,69 @@
 import React from "react";
-import { Navbar, Nav, NavbarBrand, NavItem } from "reactstrap";
+import {
+  Navbar,
+  Nav,
+  NavbarBrand,
+  NavItem,
+  NavbarToggler,
+  Collapse
+} from "reactstrap";
 
 import StyledLink from "../components/StyledLink";
 
-const Footer = () => {
-  const logout = () => {
+export default class Footer extends React.Component {
+  state = { isOpen: false };
+
+  logout = () => {
     localStorage.removeItem("jwtToken");
     window.location.reload();
   };
 
-  return (
-    <Navbar
-      color="light"
-      light
-      expand="sm"
-      fixed="bottom"
-      className="paws-footer"
-    >
-      <NavbarBrand className="paws-footer__brand">
-        Copyright © Anything Is Pawsible 2015-2019
-      </NavbarBrand>
-      <Nav className="ml-auto" navbar>
-        {localStorage.getItem("jwtToken") && (
-          <NavItem>
-            <button
-              title="Log out"
-              style={{ backgroundColor: "transparent", border: "none" }}
-              className="nav-link"
-              onClick={logout}
-            >
-              <span class="fas fa-sign-out-alt" />
-            </button>
-          </NavItem>
-        )}
-        <NavItem>
-          <StyledLink to="/admin">
-            <span className="fas fa-cog" />
-          </StyledLink>
-        </NavItem>
-        <NavItem>
-          <a
-            href="mailto:ashley.ansell@gmail.com"
-            title="Contact Admin"
-            className="nav-link"
-          >
-            <span className="fas fa-envelope" />
-          </a>
-        </NavItem>
-      </Nav>
-    </Navbar>
-  );
-};
+  toggle = () => this.setState(state => ({ isOpen: !state.isOpen }));
 
-export default Footer;
+  render() {
+    return (
+      <Navbar
+        color="light"
+        light
+        expand="md"
+        fixed="bottom"
+        className="paws-footer"
+      >
+        <NavbarBrand className="paws-footer__brand">
+          Copyright © Anything Is Pawsible 2015-2019
+        </NavbarBrand>
+        <NavbarToggler onClick={this.toggle} />
+        <Collapse isOpen={this.state.isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            {localStorage.getItem("jwtToken") && (
+              <NavItem>
+                <button
+                  title="Log out"
+                  style={{ backgroundColor: "transparent", border: "none" }}
+                  className="nav-link"
+                  onClick={this.logout}
+                >
+                  <span class="fas fa-sign-out-alt" />
+                </button>
+              </NavItem>
+            )}
+            <NavItem>
+              <StyledLink to="/admin">
+                <span className="fas fa-cog" />
+              </StyledLink>
+            </NavItem>
+            <NavItem>
+              <a
+                href="mailto:ashley.ansell@gmail.com"
+                title="Contact Admin"
+                className="nav-link"
+              >
+                <span className="fas fa-envelope" />
+              </a>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    );
+  }
+}
